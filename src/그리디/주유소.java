b4af1de2d607fe4bce487427766c294c68880c9e
jis22u <jis22u@gmail.com>
2022-11-14 21:32:17 +0900

    algo

--- src/그리디/주유소.java
+++ src/그리디/주유소.java
@@ -3,6 +3,8 @@ package 그리디;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class 주유소 {
 
@@ -10,36 +12,38 @@ public class 주유소 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
-		int[] roads = new int[N-1];
-		String str = br.readLine().replace(" ","");
+		long[] roads = new long[N-1];
+		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N-1; i++) {
-			roads[i] = str.charAt(i)-'0';
-		}
-		int[] price = new int[N];
-		str = br.readLine().replace(" ","");
-		for(int i=0; i<N; i++) {
-			price[i] = str.charAt(i)-'0';
+			roads[i] = Long.parseLong(st.nextToken());
 		}
 		
-		int len = 0;
-		int ans = 0; 
-		int result = 0;
-		int past = 1000000001;
-		for(int i=N-2; i>=0; i--) {
-			int now = price[i];
-			
-			if(now <= past) {
-				len += roads[i];
-				ans = now * len;
-			} else {
-				len = roads[i];
-				ans = now * len;
-			}
-			past = now;
-			
+		
+		long[] city = new long[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			city[i] = Long.parseLong(st.nextToken());
 		}
-		System.out.println(ans);
+		
+		long[] sorted = new long[N];
+		sorted[0] = city[0];
+		long min = city[0];
+		for(int i=1; i<N; i++) {
+			if(min > city[i]) {
+				min = city[i];
+				sorted[i] = min;
+			} else {
+				sorted[i] = min;
+			}
+		}
+		
+		long cost = 0;
+		for(int i=0; i<N-1; i++) {
+			cost += (sorted[i] * roads[i]);
+		}
+		System.out.println(cost);
 		
 	} //main
 
