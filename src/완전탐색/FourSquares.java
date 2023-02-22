e02b5370983d8821d6dd17a7d50991109637d452
jis22u <jis22u@gmail.com>
2023-02-23 00:06:56 +0900

    algo 0222

--- src/완전탐색/FourSquares.java
+++ src/완전탐색/FourSquares.java
@@ -2,10 +2,12 @@ package 완전탐색;
 
 import java.util.Scanner;
 
+
+// 시간초과
 public class FourSquares {
 
 	static int[] arr;
-	static int cnt, n, m, ans;
+	static int n, m, ans;
 	static boolean[] visited;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -13,7 +15,6 @@ public class FourSquares {
 		
 		// 정수 m까지 완전탐색을 할거야
 		m = (int) Math.sqrt(n);
-		System.out.println(m);
 		
 		
 		// 1부터 m까지의 수가 저장된 배열
@@ -27,13 +28,7 @@ public class FourSquares {
 		// 방문처리
 		visited = new boolean[m];
 		
-		// 1개부터 m개까지 숫자를 선택할거야
-//		for(int i = 0; i<m; i++) {
-//			// 선택할 정수의 개수
-//			cnt = i+1;
-			// 제곱수의 합, 선택된 정수의 개수
-			search(0, 0);
-//		}
+		search(0, 0);
 		
 		System.out.println(ans);
 		
@@ -44,7 +39,7 @@ public class FourSquares {
 		if(res == n) {
 			ans = Math.min(sCnt, ans);
 			return;
-		} else if(sCnt == m) return;
+		} else if(sCnt == 4) return;
 		
 	
 		for(int i = 0; i<m; i++) {
