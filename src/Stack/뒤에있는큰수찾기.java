13f0d672327b3a1d3c188a7a727002f89d7aedf1
jis22u <jis22u@gmail.com>
2023-03-26 00:32:36 +0900

    algo 0325

--- src/Stack/뒤에있는큰수찾기.java
+++ src/Stack/뒤에있는큰수찾기.java
@@ -14,21 +14,21 @@ public class 뒤에있는큰수찾기 {
 
 		for(int i=numbers.length-1; i>=0; i--) {
 		    while(!stack.isEmpty()) {
-			// peek이랑 비교
-			int target = stack.peek();
-			// peek이 더 크다면 뒷큰수
-			if(numbers[i] < target) {
-			    answer[i] = target;
-			    break;
-			} 
-			// 해당 정수가 더 크다면 스택에 있는거 pop 해버리기
-			else {
-			    stack.pop();
-			}
+				// peek이랑 비교
+				int target = stack.peek();
+				// peek이 더 크다면 뒷큰수
+				if(numbers[i] < target) {
+				    answer[i] = target;
+				    break;
+				} 
+				// 해당 정수가 더 크다면 스택에 있는거 pop 해버리기
+				else {
+				    stack.pop();
+				}
 		    }
 		    // 스택에 쌓인거 다 꺼내봐도 없다면 뒷큰수가 없는 것
 		    if(stack.isEmpty()) {
-			answer[i] = -1;
+		    	answer[i] = -1;
 		    }
 		    stack.push(numbers[i]);
 		}
