3bb7db1590651b71ee50b6b61fdc803d24002f2b
jis22u <110139421+jis22u@users.noreply.github.com>
2023-04-16 17:51:27 +0900

    Update 최댓값과최솟값.java

--- src/구현/최댓값과최솟값.java
+++ src/구현/최댓값과최솟값.java
@@ -7,17 +7,17 @@ public class 최댓값과최솟값 {
 	public static void main(String[] args) {
 		String s = "-1 -2 -3 -4";
 		// 문자열을 배열로 받아주기
-        String[] c = s.split(" ");
-        int[] num = new int[c.length];
-        // 문자열을 정수형 숫자로 변환
-        for(int i=0; i<c.length; i++) {
-            num[i] = Integer.parseInt(c[i]);
-        }
-        // 정렬
-        Arrays.sort(num);
-        // 정답 출력
-        String answer = num[0]+" "+num[num.length-1];
-        System.out.println(answer);
+		String[] c = s.split(" ");
+		int[] num = new int[c.length];
+		// 문자열을 정수형 숫자로 변환
+		for(int i=0; i<c.length; i++) {
+		    num[i] = Integer.parseInt(c[i]);
+		}
+		// 정렬
+		Arrays.sort(num);
+		// 정답 출력
+		String answer = num[0]+" "+num[num.length-1];
+		System.out.println(answer);
 		
 	}
 
