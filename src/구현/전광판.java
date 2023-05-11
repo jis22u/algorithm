c19e1cd688069c19782261e3f34398ee04437d86
jis22u <jis22u@gmail.com>
2023-05-11 09:08:56 +0900

    algo 0511

---
+++ src/구현/전광판.java
@@ -0,0 +1,79 @@
+package 구현;
+
+import java.util.*;
+import java.io.*;
+
+public class 전광판 {
+
+	public static void main(String[] args) throws Exception {
+		// 0-9까지 전광판 현황 저장
+        boolean[][] numNeon = {
+            {true, true, true, true, true, true, false},  // 0 
+            {false, false, true, true, false, false, false},    // 1
+            {false, true, true, false, true, true, true},       // 2
+            {false, true, true, true, true, false, true},       // 3
+            {true, false, true, true, false, false, true},      // 4
+            {true, true, false, true, true, false, true},       // 5
+            {true, true, false, true, true, true, true},        // 6
+            {true, true, true, true, false, false, false},      // 7
+            {true, true, true, true, true, true, true},         // 8
+            {true, true, true, true, true, false, true}         // 9
+        };
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        // int[] A = new int[5];
+        // int[] B = new int[5];
+        for(int t=0; t<T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String a = st.nextToken();
+            String b = st.nextToken();
+            int alen = a.length();
+            int blen = b.length();
+            int maxlen = alen;
+            int minlen = blen;
+            String minStirng = a;
+            if(alen < blen) {
+                maxlen = blen;
+                minlen = alen;
+                minStirng = a;
+            } else {
+                maxlen = alen;
+                minlen = blen;
+                minStirng = b;
+            }
+
+            int ans = 0;
+            for(int i=minlen; i>=0; i--) {
+//                if(
+//                        numNeon[Integer.parseInt(""+a.charAt(i))][j] != 
+//                        numNeon[Integer.parseInt(""+b.charAt(i))][j]
+//                    ) {
+//                        ans++;
+//                    }
+            }
+
+//            for(int i=maxlen; ) {
+//
+//            }
+            // System.out.println(a);
+            // System.out.println(b);
+
+
+            for(int i=0; i<5; i++){
+                for(int j=0; j<7; j++) {
+                    if(
+                        numNeon[Integer.parseInt(""+a.charAt(i))][j] != 
+                        numNeon[Integer.parseInt(""+b.charAt(i))][j]
+                    ) {
+                        ans++;
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
+
+	}
+
+}
