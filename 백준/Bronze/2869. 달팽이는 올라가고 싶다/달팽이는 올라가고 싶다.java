d1ba6c88e1795c2ce2c8498d396d168486a1614c
JISU KANG <jis22u@gmail.com>
2024-02-27 16:13:48 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken()); 
+        int V = Integer.parseInt(st.nextToken());       
+
+        
+        int days = (V - B) / (A - B);
+        if((V - B) % (A - B) != 0) days++;
+
+        System.out.println(days);
+        
+    }
+}
