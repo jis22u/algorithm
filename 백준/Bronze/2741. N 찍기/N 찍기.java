1168dfad0be428aebfdc8d8e66323998d402abaa
JISU KANG <jis22u@gmail.com>
2024-02-27 17:34:27 +0900

    [Bronze V] Title: N 찍기, Time: 1604 ms, Memory: 36036 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+      
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++) {
+            System.out.println(i);
+        }
+    }
+}
